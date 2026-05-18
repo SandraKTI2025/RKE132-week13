64d5ae7ba9b616889e84232a8f6734faa25c88d9
SandraKTI2025 <sandra.savinski@tktk.ee>
2026-05-18 03:51:16 +0300

    Update animal.py

--- animal.py
+++ animal.py
@@ -3,9 +3,9 @@ class Animal:
         self.name = pet_name
         self.ears = 2
         self.legs = 4
-    
+
     def sees(self, other):
-        print(f"{self.name} sees {other.name}.")
+        print(f"{self.name} sees {other.name}")
 
 
 class Cat(Animal):
@@ -18,8 +18,7 @@ class Cat(Animal):
 
     def sleep(self):
         print(f"{self.name} is sleeping...")
-
-        self.hunger = self.hunger + 1
+        self.hunger += 1
 
         if self.hunger >= 10:
             print(f"{self.name} wakes up hungry!")
@@ -28,8 +27,14 @@ class Cat(Animal):
     def hiss(self):
         print(f"{self.name}: Hissss!")
 
-    def cat_sees(self):
-    #[your code here]
+    def cat_sees(self, other):
+        super().sees(other)
+
+        if isinstance(other, Dog):
+            self.hiss()      
+        else:
+            self.meow()     
+
 
 class Dog(Animal):
     def __init__(self, dog_name):
@@ -39,7 +44,7 @@ class Dog(Animal):
         print(f"{self.name}: Woof!")
 
     def wag_the_tail(self):
-        print(f"{self.name} is wagging it tail.")
+        print(f"{self.name} is wagging its tail")
 
     def dog_sees(self, other):
         super().sees(other)
@@ -48,4 +53,3 @@ class Dog(Animal):
             self.bark()
         else:
             self.wag_the_tail()
-
